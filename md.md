01c6df6312d669f04fa21bc4f9dde24e44109a48
crayon <fuheyong1@100tal.com>
2021-03-30 19:55:15 +0800

    feat: 环境搭建

---
+++ md.md
@@ -0,0 +1,9 @@
+- npm install -g typescript
+<!-- 生成tsc命令 -->
+<!-- tsc --init 生成配置文件 -->
+- tsc --init
+<!-- 安装ts-node，可以查看ts的转成js，查看输出 -->
+- npm install -g ts-node
+<!-- 通过 rollup构建工具-->
+- npm install rollup typescript rollup-plugin-typescript2 @rollup/plugin-node-resolve rollup-plugin-serve -D
+<!-- 创建rollup.config.js配置 -->
---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import ts from 'rollup-plugin-typescript2' //解析ts插件
+import { nodeResolve } from '@rollup/plugin-node-resolve' //解析第三方插件
+import serve from 'rollup-plugin-serve' //启动本地服务插件
+import path from 'path'
+export default{
+  input:'src/index.ts',
+  output:{
+    format:'iife', //输出的文件类型， iife:一个自动执行的功能
+    file:path.resolve(__dirname,'dist/bundle.js'),//输出文件
+    sourcemap:true, //根据源码产生映射文件
+  },
+  plugins:[
+    nodeResolve({
+      extensions:['.js','.ts']
+    }),
+    ts({
+      tsconfig:path.resolve(__dirname,'tsconfig.json')
+    }),
+    serve({
+      openPage:'public/index.html',
+      contentBase:'',
+      port:9090
+    })
+  ]
+}
